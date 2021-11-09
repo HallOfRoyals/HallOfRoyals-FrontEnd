dd05e94223d167668b73463d648873e40c40ad47
Confidence Yobo <confiyobo@gmail.com>
2021-11-09 05:12:08 -0600

    Change showcase pattern

--- src/components/ShowCase.js
+++ src/components/ShowCase.js
@@ -1,88 +1,30 @@
-import React, { useLayoutEffect, useState } from 'react';
 import '../static/css/ShowCase.css';
 import pic1 from '../static/images/showcase/1.png';
 import pic2 from '../static/images/showcase/2.png';
-import pic3 from '../static/images/showcase/3.png';
-import pic4 from '../static/images/showcase/4.png';
-import pic5 from '../static/images/showcase/5.png';
-import pic6 from '../static/images/showcase/6.png';
-import pic7 from '../static/images/showcase/7.png';
-import pic8 from '../static/images/showcase/8.png';
-import pic9 from '../static/images/showcase/9.png';
-import pic10 from '../static/images/showcase/10.png';
-import pic11 from '../static/images/showcase/11.png';
-import pic12 from '../static/images/showcase/12.png';
-import pic13 from '../static/images/showcase/13.png';
-import pic14 from '../static/images/showcase/14.png';
-import pic15 from '../static/images/showcase/15.png';
-import pic16 from '../static/images/showcase/16.png';
-import ShowCaseSlider from './ShowCaseSlider';
-
-const slides = [
-  { "image": pic1 },
-  { "image": pic2 },
-  { "image": pic3 },
-  { "image": pic4 },
-  { "image": pic5 },
-  { "image": pic6 },
-  { "image": pic7 },
-  { "image": pic8 },
-  { "image": pic9 },
-  { "image": pic10 },
-  { "image": pic11 },
-  { "image": pic12 },
-  { "image": pic13 },
-  { "image": pic14 },
-  { "image": pic15 },
-  { "image": pic16 },
-];
-
-const settings = {
-  dots: true,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 3.4,
-  slidesToScroll: 1,
-  className: "center",
-  centerMode: true,
-  autoplay: true,
-  speed: 500,
-  autoplaySpeed: 3000,
-  swipeToSlide: true,
-  pauseOnHover: true,
-  cssEase: "linear"
-}
-
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-  return size;
-}
+import ShowCaseSlider from './SingleShowcase';
 
 export default function ShowCase() {
-  const width = useWindowSize()[0];
-  if (width > 0) {
-    if (width <= 800 && width > 510) {
-      settings.slidesToShow = 2;
-      return (<section className="showcase pb-50">
-        <ShowCaseSlider settings={settings} slides={slides} />
-      </section>);
-    } else if (width <= 510) {
-      settings.slidesToShow = 1;
-      return (<section className="showcase pb-50">
-        <ShowCaseSlider settings={settings} slides={slides} />
-      </section>);
+  const showcases = [
+    {
+      image: pic1,
+      title: 'Ancient Royals',
+      helper: 'The First Dynasties',
+      story: <div><p>Adorned with ancient ornaments these Royals look stunning</p> <p>They are built from materials known from the the 16th century upwards</p></div>,
+    },
+    {
+      image: pic2,
+      title: 'Meta Royals',
+      helper: 'Modern Dynasties',
+      story: <div><p>Stunning just like their ancestors these Meta Royals throw in a touch of modern artistic style, using accessories from the Metaverse world</p></div>,
     }
-  }
-
+  ];
   return (<section className="showcase pb-50">
-    <ShowCaseSlider settings={settings} slides={slides} />
+    <div className="container">
+      <div className="row align-items-center text-center case">
+        {showcases.map((showcase, index) => (
+          <ShowCaseSlider key={index} showcase={showcase} />
+        ))}
+      </div>
+    </div>
   </section>);
 }
---
+++ src/components/SingleShowcase.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+
+export default function SingleShowCase(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClick = (event) => {
+    setShow(!show);
+  };
+
+  return (
+    <div className="col-sm-6 magic-mt-30">
+      <img src={props.showcase.image} alt="" />
+      <h2 className="mt-25 --mb-5">{props.showcase.title}</h2>
+      <h3 className="--mb-5">{props.showcase.helper}</h3>
+      <OverlayTrigger
+        show={show}
+        placement='bottom'
+        overlay={
+          <Popover id={`popover-positioned-${props.key}`}>
+            <Popover.Body>
+              {props.showcase.story}
+            </Popover.Body>
+            <div className="d-flex justify-content-end --close-dialog">
+              <Button onClick={handleClick}>[close]</Button>
+            </div>
+          </Popover>
+        }
+      >
+        <Button variant="secondary" onClick={handleClick}>view story</Button>
+      </OverlayTrigger>
+    </div>
+  );
+}
