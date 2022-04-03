455dc91c64f9b8461de89def5fb21e63b14036a9
Confidence Yobo <confiyobo@gmail.com>
2022-04-03 13:29:00 +0100

    Update roadmap texts

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -10,25 +10,13 @@ export default function RoadMap() {
         <div className="row mt-25 align-items-start">
           <div className="col-md-6">
             <div className="glassy-wrapper relative-height-box pt-20 pb-20 pl-20 pr-20">
-              <p>Completion of the 1000 Genesis collection</p>
-              <p className="mt-15">Adding more Legendary Royals to the Genesis collection</p>
-              <p className="mt-15">Sculpting and onboarding more celebrities into the ‘Hall of Royals’</p>
+              <p>Sculpting and onboarding more celebrity and achievers into the "Hall Of Royals"</p>
+              <p className="mt-15">Collaboration with award organizers such as Headies Awards, BET Awards, etc. to create 3D Hall Of Royals NFT sculpts for their winners</p>
+              <p className="mt-15">Create "Hall Of Royals Awards" to recognise and celebrate achievers in blockchain and metaverse</p>
               <p className="mt-15">Sculpting minted custom-sculpts which are also part of the
                 Genesis collection</p>
-              <p className="mt-15">Collaboration with Artists around the world to create more unique and
-                special Royals, ornaments & accessories</p>
+              <p className="mt-15">Collaborate with Artists around the world to create concepts Arts using the Benin Bronze Heads Art form</p>
             </div>
-            <ul className="roadmap-ul mt-60">
-              <li>
-                All Hall Of Royals NFT holders get a 3D file of their Royal in the requested format
-              </li>
-              <li>
-                All holders can also request and get a 4K image of their Royal
-              </li>
-              <li>
-                Early access & air drops of future NFT drops
-              </li>
-            </ul>
           </div>
           <div className="col-md-6 text-right">
             <img src={roadmapArt} className="d-none d-md-inline-block roadmap-art" alt=""/>
