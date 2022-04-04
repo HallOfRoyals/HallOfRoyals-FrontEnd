c431f54f04cf0ea2373ec5fb7b88ef729b5dae56
Confidence Yobo <confiyobo@gmail.com>
2022-04-04 13:19:33 +0100

    Update roadmap texts

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -6,14 +6,14 @@ export default function RoadMap() {
     <section id="roadmap" className="glassy-wrapper x-section mt-60 position-relative">
       <img src={shapeSixteen} alt="" className="shape-16" />
       <div className="container">
-        <h1 className="section-title">Royals</h1>
+        <h1 className="section-title">Roadmap</h1>
         <div className="row mt-25 align-items-start">
           <div className="col-md-6">
             <div className="glassy-wrapper relative-height-box pt-20 pb-20 pl-20 pr-20">
               <p>Sculpting and onboarding more celebrity and achievers into the "Hall Of Royals"</p>
               <p className="mt-15">Collaboration with award organizers such as Headies Awards, BET Awards, etc. to create 3D Hall Of Royals NFT sculpts for their winners</p>
-              <p className="mt-15">Create "Hall Of Royals Awards" to recognise and celebrate achievers in blockchain and metaverse</p>
-              <p className="mt-15">Partner with celebrities and other NFt projects. We will handle the creation of their collections in 3D using our NFT attributes while our community get perks from them</p>
+              <p className="mt-15">Create "Hall Of Royals Awards" to recognise and celebrate achievers in the blockchain/metaverse</p>
+              <p className="mt-15">Partnering with celebrities and other projects to create their NFT collection using our assets and attributes with our community gaining perks as a result</p>
               <p className="mt-15">Collaborate with Artists around the world to create concepts Arts using the Benin Bronze Heads Art form</p>
             </div>
           </div>
