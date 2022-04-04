0d1b9567b77f0061d919867f7eddbc946e00a86e
Confidence Yobo <confiyobo@gmail.com>
2022-04-04 01:22:03 +0100

    Update roadmap text

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -13,8 +13,7 @@ export default function RoadMap() {
               <p>Sculpting and onboarding more celebrity and achievers into the "Hall Of Royals"</p>
               <p className="mt-15">Collaboration with award organizers such as Headies Awards, BET Awards, etc. to create 3D Hall Of Royals NFT sculpts for their winners</p>
               <p className="mt-15">Create "Hall Of Royals Awards" to recognise and celebrate achievers in blockchain and metaverse</p>
-              <p className="mt-15">Sculpting minted custom-sculpts which are also part of the
-                Genesis collection</p>
+              <p className="mt-15">Partner with celebrities and other NFt projects. We will handle the creation of their collections in 3D using our NFT attributes while our community get perks from them</p>
               <p className="mt-15">Collaborate with Artists around the world to create concepts Arts using the Benin Bronze Heads Art form</p>
             </div>
           </div>
